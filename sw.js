40ab36e6d95b8178a5ef2bbabf78c740845a5fd8
brucewu9207-cell <bruce.wu9207@gmail.com>
2026-06-21 19:07:49 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 斑斓汉字(老师端) · 离线缓存引擎(service worker) */
-const CACHE = 'banlan-hanzi-v42';
+const CACHE = 'banlan-hanzi-v43';
 const CORE = ['./', './index.html', './manifest.webmanifest',
   './icon-192.png', './icon-512.png', './apple-touch-icon-180.png'];
 
